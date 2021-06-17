51043a4c8e0b133ef23612e3d90f60aace908100
BceOK <artrozhko@gmail.com>
2021-06-17 15:31:56 +0300

    ДЗ №1

--- src/main/java/cloudstorage/homework01/ClientHandler.java
+++ src/main/java/cloudstorage/homework01/ClientHandler.java
@@ -1,9 +1,6 @@
 package cloudstorage.homework01;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.net.Socket;
 
 public class ClientHandler implements Runnable {
@@ -23,6 +20,7 @@ public class ClientHandler implements Runnable {
 			System.out.printf("Client %s connected\n", socket.getInetAddress());
 			while (true) {
 				String command = in.readUTF();
+				String status = "";
 				if ("upload".equals(command)) {
 					try {
 						File file = new File("server"  + File.separator + in.readUTF());
@@ -35,27 +33,54 @@ public class ClientHandler implements Runnable {
 
 						byte[] buffer = new byte[8 * 1024];
 
+//						т.к. размер файла зачастую некратен нашему буферу байт,
+//						то прибавляем размер нашего буффера минус 1 (-1 чтобы избежать лишней итерации цикла,
+//						если размер файла будет кратен буферу) к размеру файла при подсчете количества
+//						целых шагов цикла
 						for (int i = 0; i < (size + (buffer.length - 1)) / (buffer.length); i++) {
 							int read = in.read(buffer);
 							fos.write(buffer, 0, read);
 						}
 						fos.close();
-						out.writeUTF("OK");
+						status = "OK";
 					} catch (Exception e) {
-						out.writeUTF("FATAL ERROR");
+						status = "FATAL ERROR";
 					}
 				}
 
 				if ("download".equals(command)) {
-					// TODO: 14.06.2021
+					try {
+						File file = new File("server"  + File.separator + in.readUTF());
+						if (!file.exists()) {
+							throw  new FileNotFoundException();
+						}
+
+						long fileLength = file.length();
+
+						FileInputStream fis = new FileInputStream(file);
+
+						out.writeLong(fileLength);
+
+						int read = 0;
+						byte[] buffer = new byte[8 * 1024];
+						while ((read = fis.read(buffer)) != -1) {
+							out.write(buffer, 0, read);
+						}
+						out.flush();
+
+						status = in.readUTF();
+
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
 				}
 				if ("exit".equals(command)) {
 					System.out.printf("Client %s disconnected correctly\n", socket.getInetAddress());
 					break;
 				}
 
-				System.out.println(command);
-				out.writeUTF(command);
+				System.out.printf("%s command status: %s\n", command, status);
+				out.writeUTF(status);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
